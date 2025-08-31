aea617f1eb7574307df0e8da983570d2e83e12d2
devmork <enoymarkden@gmail.com>
2025-08-31 20:50:11 +0800

    Add files

---
+++ Javascript/03-functions/boolean-checker.js
@@ -0,0 +1,15 @@
+// Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+// User Stories:
+
+// You should have a function called booWho that receives one argument.
+// If the argument received is a boolean primitive, the function should return true.
+// If the argument is any other value, the function should return false.
+
+function booWho(value) {
+  if (value === true || value === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
